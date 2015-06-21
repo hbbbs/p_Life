4b4ef3821dffd30fb33c719a20ef2bec147646c4
hbbbs <hbbbs123456@126.com>
2015-06-21 23:40:39 +0800

    天气获取

--- src/services/weather/http/HttpRequestUtils.java
+++ src/services/weather/http/HttpRequestUtils.java
@@ -1,14 +1,19 @@
 package services.weather.http;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 
 /**
@@ -19,10 +24,11 @@ public class HttpRequestUtils {
 
     /**
      * 发送get请求
-     * @param url    路径
+     *
+     * @param url 路径
      * @return
      */
-    public static String httpGet(String url){
+    public static String httpGet(String url) {
         //get请求返回结果
         String jsonResult = null;
         try {
@@ -34,7 +40,7 @@ public class HttpRequestUtils {
             /**请求发送成功，并得到响应**/
             if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
                 /**读取服务器返回过来的json字符串数据**/
-                jsonResult = EntityUtils.toString(response.getEntity());
+                jsonResult = EntityUtils.toString(response.getEntity(), "UTF-8");
                 /**把json字符串转换成json对象**/
 //                jsonResult = JSONObject.fromObject(strResult);
 //                url = URLDecoder.decode(url, "UTF-8");
@@ -44,6 +50,51 @@ public class HttpRequestUtils {
         } catch (IOException e) {
             logger.error("get请求提交失败:" + url, e);
         }
-        return jsonResult;42343242
+        return jsonResult;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(HttpRequestUtils.httpGet1("http://wthrcdn.etouch.cn/weather_mini?city=株洲"));
+    }
+
+    public static String httpGet1(String url) {
+        String responseContent = null;
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpGet httpGet = new HttpGet(url);
+            CloseableHttpResponse response1 = httpclient.execute(httpGet);
+            try {
+                HttpEntity entity = response1.getEntity();
+                responseContent = getRespString(entity);
+                EntityUtils.consume(entity);
+            } finally {
+                response1.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                httpclient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return responseContent;
+    }
+
+    private static String getRespString(HttpEntity entity) throws Exception {
+        if (entity == null) {
+            return null;
+        }
+        InputStream is = entity.getContent();
+        StringBuffer strBuf = new StringBuffer();
+        byte[] buffer = new byte[4096];
+        int r = 0;
+        while ((r = is.read(buffer)) > 0) {
+            strBuf.append(new String(buffer, 0, r, "UTF-8"));
+        }
+        return strBuf.toString();
     }
 }
+
+
